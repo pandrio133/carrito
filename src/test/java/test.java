b6fc853d6e7ebb705e9e633c0aa3047d92a3ea3e
pandrio133 <pandrio133@gmail.com>
2024-11-04 17:21:43 -0500

    testeo de conexion a bbdd

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,23 +1,12 @@
 
-import Modelo.Cliente;
+import Conexion.Conexion;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author usuario
- */
 public class test {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       Cliente c1=new Cliente(11,"correo", "nombre",111);
-        c1.guadarDatos(c1);
+       
+        Conexion con= new Conexion();
+        con.Conexion();
     }
     
 }
