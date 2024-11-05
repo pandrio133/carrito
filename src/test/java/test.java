56efa5debf65cfe636834de1b98438527cc6b09e
pandrio133 <pandrio133@gmail.com>
2024-11-05 17:36:15 -0500

    haciendo prueba del metodo guardarDatos de cliente, instacio un cliente

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,12 +1,17 @@
 
 import Conexion.Conexion;
+import Modelo.Cliente;
 
 public class test {
 
     public static void main(String[] args) {
-       
-        Conexion con= new Conexion();
-        con.Conexion();
+//       
+//        Conexion con= new Conexion();
+//        con.Conexion(); 
+        
+        Cliente nvC = new Cliente(123, "correo", "nombre", 123);
+        
+        nvC.guadarDatos(nvC);
     }
     
 }
