abd27dd1a5ad67ada77dfe4fd0a0b9166f9be716
pandrio133 <pandrio133@gmail.com>
2024-11-11 23:10:21 -0500

    La base de todo los "crud" va ha ser ahora la clase conexion

--- src/main/java/Conexion/Conexion.java
+++ src/main/java/Conexion/Conexion.java
@@ -3,25 +3,33 @@ package Conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Conexion {
     
-    private String bd = "bd_carrito";
-    private String user = "root";
-    private String pass = "";
-    private String url = "jdbc:mysql://localhost:3306/" + bd;
+    private final String bd = "ventas_mg";
+    private final String user = "root";
+    private final String pass = "";
+    private final String url = "jdbc:mysql://localhost:3306/" + bd;
+    
+    //variables de consulta Statement y ResultSet
+    private Statement st;
+    private ResultSet rs;
 
     public Conexion() {
     }
     
     private Connection con;
-    public Connection Conexion(){
+    public Connection Conectar(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             
             con=DriverManager.getConnection(url, user, pass);
             System.out.println("conectado");
+            
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println(e);
             System.out.println("error");
@@ -37,6 +45,31 @@ public class Conexion {
             System.err.println(e);
         }
     }
+    
+    // C-R-U-D
+    
+    //create
+    public void Create( String tabla){
+        
+        String query = "select * from "+tabla+" ;";
+
+        try {
+            st = Conectar().createStatement();
+            rs = st.executeQuery(query);
+            System.out.println("Ingreso realizado correctamente.");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
+    }
+    
+    //read
+    
+    
+    //update
+    
+    
+    //delete
     
     
 }
