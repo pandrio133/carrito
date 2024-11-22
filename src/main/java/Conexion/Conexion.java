41a2d3a3115e0cc8ad1fc7584c9174d6571162cc
pandrio133 <pandrio133@gmail.com>
2024-11-22 13:00:19 -0500

    optimizacion del primer metodo read de y agrege un read con where

--- src/main/java/Conexion/Conexion.java
+++ src/main/java/Conexion/Conexion.java
@@ -70,150 +70,71 @@ public class Conexion {
         
         String query = "select * from "+tabla+" ;";
         
-        String c_1,c_2,c_3,c_4,c_5,c_6;
+        String col;
+        ArrayList<String> re = new ArrayList<String>();
+        
+        try {
+            st = Conectar().createStatement();
+            rs = st.executeQuery(query);
+            int i=1;
+            while(rs.next()) {
+                while (i<=NColumnas) {                    
+                    col = rs.getString(i);
+                        re.add(col);
+                        ++i;
+                }
+                i=1;
+            }
+            
+            
+            System.out.println("Consulta realizado correctamente.");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
+        return re;
+    }
+//------------------------------------------------------------------------------  
+    public ArrayList Read (String tabla, String columna){
+        String query = "select "+columna+" from "+tabla+" ;";
+        
+        String col;
         ArrayList<String> re = new ArrayList<String>();
         
         try {
             st = Conectar().createStatement();
             rs = st.executeQuery(query);
             
-            switch (NColumnas) {
-                case 1:
-                    while (rs.next()) {                
-                        c_1 = rs.getString(1);
-
-                        System.out.println(
-                            c_1+"\n"+
-                            "-------"
-                        );
+           while (rs.next()) {                
+                        col = rs.getString(1);
                         
-                        re.add(c_1);
-                        re.add("------------");
+                        re.add(col);
                     }
-                    break;
-                case 2:
-                    while (rs.next()) {                
-                        c_1 = rs.getString(1);
-                        c_2 = rs.getString(2);
-
-                        System.out.println(
-                            c_1+"\n"+
-                            c_2+"\n"+
-                            "-------"
-                        );
-                        re.add(c_1);
-                        re.add(c_2);
-                        re.add("------------");
-                    }
-                    break;
-                case 3:
-                    while (rs.next()) {                
-                        c_1 = rs.getString(1);
-                        c_2 = rs.getString(2);
-                        c_3 = rs.getString(3);
-
-                        System.out.println(
-                            c_1+"\n"+
-                            c_2+"\n"+
-                            c_3+"\n"+
-                            "-------"
-                        );
-                        re.add(c_1);
-                        re.add(c_2);
-                        re.add(c_3);
-                        re.add("------------");
-                    }
-                     
-                    break;
-                case 4:
-                    while (rs.next()) {                
-                        c_1 = rs.getString(1);
-                        c_2 = rs.getString(2);
-                        c_3 = rs.getString(3);
-                        c_4 = rs.getString(4);
-
-                        System.out.println(
-                            c_1+"\n"+
-                            c_2+"\n"+
-                            c_3+"\n"+
-                            c_4+"\n"+
-                            "-------"
-                        );
-                        re.add(c_1);
-                        re.add(c_2);
-                        re.add(c_3);
-                        re.add(c_4);
-                        re.add("------------");
-                    }
-                    
-                    break;
-                case 5:
-                    while (rs.next()) {                
-                        c_1 = rs.getString(1);
-                        c_2 = rs.getString(2);
-                        c_3 = rs.getString(3);
-                        c_4 = rs.getString(4);
-                        c_5 = rs.getString(5);
-
-                        System.out.println(
-                            c_1+"\n"+
-                            c_2+"\n"+
-                            c_3+"\n"+
-                            c_4+"\n"+
-                            c_5+"\n"+
-                            "-------"
-                        );
-                        
-                        re.add(c_1);
-                        re.add(c_2);
-                        re.add(c_3);
-                        re.add(c_4);
-                        re.add(c_5);
-                        re.add("------------");
-                    }
-                    break;
-                case 6:
-                    while (rs.next()) {                
-                        c_1 = rs.getString(1);
-                        c_2 = rs.getString(2);
-                        c_3 = rs.getString(3);
-                        c_4 = rs.getString(4);
-                        c_5 = rs.getString(5);
-                        c_6 = rs.getString(6);
-
-                        System.out.println(
-                            c_1+"\n"+
-                            c_2+"\n"+
-                            c_3+"\n"+
-                            c_4+"\n"+
-                            c_5+"\n"+
-                            c_6+"\n"+
-                            "-------"
-                        );
-                        
-                        re.add(c_1);
-                        re.add(c_2);
-                        re.add(c_3);
-                        re.add(c_4);
-                        re.add(c_5);
-                        re.add(c_6);
-                        re.add("------------");
-                    }
-                    break;
-                default:
-                      while (rs.next()) {                
-                        c_1 = rs.getString(1);
-                        System.out.println(
-                            c_1+"\n"+
-                            "-------"
-                             
-                        );
-                        re.add(c_1);
-                        re.add("------------");
-                      }
-                     
-                    break;
-                    
+            System.out.println("Consulta realizado correctamente.");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
+        return re;
+    }
+//------------------------------------------------------------------------------
+    public ArrayList Read (String tabla,int NColumnas ,String columnaWhere, String dato){
+        String query = "select * from "+tabla+" where "+columnaWhere+"="+dato+";";
+        
+        String col;
+        ArrayList<String> re = new ArrayList<String>();
+        
+        try {
+            st = Conectar().createStatement();
+            rs = st.executeQuery(query);
+            int i=1;
+            while(rs.next()) {
+                while (i<=NColumnas) {                    
+                    col = rs.getString(i);
+                        re.add(col);
+                        ++i;
+                }
+                i=1;
             }
             System.out.println("Consulta realizado correctamente.");
         } catch (Exception e) {
@@ -222,6 +143,8 @@ public class Conexion {
         
         return re;
     }
+    
+    
     
     
     //update
